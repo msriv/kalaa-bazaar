ae0970ce770bfb418cfffc878766793c4a0e07c1
Mihir Srivastava <msrivastava574@gmail.com>
2022-11-13 15:20:44 +0530

    [Server | Client] Linter fixes and setup fixes

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,8 +1,8 @@
 module.exports = {
-  ...require("gts/.prettierrc.json"),
+  ...require('gts/.prettierrc.json'),
   semi: false,
-  trailingComma: "es5",
+  trailingComma: 'es5',
   singleQuote: true,
   tabWidth: 2,
   useTabs: false,
-};
+}
--- client/next.config.js
+++ client/next.config.js
@@ -1,7 +1,8 @@
+/* eslint-disable */
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
 }
 
-module.exports = nextConfig
+module.exports = nextConfig;
